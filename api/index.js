7a11d146e845a35aa0fab3f34c94c8b3c385535e
gartnerleandro <gartnerleandro@gmail.com>
2020-05-14 13:38:05 +0200

    Fix: CORS problem and return json on categories endpoint

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 import categories from './db/categories.json';
 
 const app = express();
 const port = 3001;
 
-app.get('/api/categories', (req, res) => res.status(200).send(categories));
+app.use(cors());
+
+app.get('/api/categories', (req, res) => res.json(categories));
 
 app.listen(port, () => console.log(`Server running on port ${port}! 🚀`));
