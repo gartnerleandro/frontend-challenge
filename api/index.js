4b2c3075b24ec1580e2dbf8e1cc176e3208563bd
gartnerleandro <gartnerleandro@gmail.com>
2020-05-15 13:47:01 +0200

    Feature: add validate email endpoint

--- api/index.js
+++ api/index.js
@@ -5,9 +5,23 @@ import categories from './db/categories.json';
 
 const app = express();
 const port = 3001;
+const emailRegex = /^([a-zA-Z0-9_.+]+)@([a-zA-Z0-9]+)(\.+[a-zA-Z]{2,})$/i;
+const hotmailRegex = /^([a-zA-Z0-9_.+]+)@(hotmail)(\.+[a-zA-Z]{2,})$/i;
 
 app.use(cors());
 
 app.get('/api/categories', (req, res) => res.json(categories));
+
+app.get('/api/validateEmail', (req, res) => {
+  if (
+    req?.query?.email
+    && emailRegex.test(req.query.email)
+    && !hotmailRegex.test(req.query.email)
+  ) {
+    return res.json({ isValid: true });
+  }
+
+  return res.json({ isValid: false });
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}! 🚀`));
